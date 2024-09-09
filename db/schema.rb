5c5aef70c10677f0bfa5e125229894f8bfc95be8
John Breen <developer@john-breen.net>
2024-09-09 20:48:23 +0800

    Add employee_id to staffs table, which is what staff_id should have been called to start with

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_09_09_122325) do
+ActiveRecord::Schema[7.0].define(version: 2024_09_09_124448) do
   create_table "allocations", force: :cascade do |t|
     t.integer "shift_id", null: false
     t.integer "staff_id", null: false
@@ -40,6 +40,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_09_09_122325) do
     t.string "last_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "employee_id"
   end
 
   add_foreign_key "allocations", "shifts"
